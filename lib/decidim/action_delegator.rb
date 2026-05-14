b9ad54e64b2256a0858dba93449dc1f081affe3d
Table en Latin <deploy@tableenlatin.fr>
2026-05-14 18:48:20 +0200

    Replace ActiveSupport::Configurable with mattr_accessor
    ActiveSupport::Configurable is deprecated in Rails 8.1 and slated for
    removal in 8.2. mattr_accessor (with default:) is the module-level
    equivalent and matches the pattern Decidim core itself migrated to.
    
    Defaults switch from lazy block evaluation to eager evaluation at module
    load, which is fine because Decidim::Env reads ENV at construction time
    and Rails loads the env file before requiring any gem code.

--- lib/decidim/action_delegator.rb
+++ lib/decidim/action_delegator.rb
@@ -9,44 +9,36 @@ require "decidim/action_delegator/engine"
 module Decidim
   # This namespace holds the logic of the `ActionDelegator` module
   module ActionDelegator
-    include ActiveSupport::Configurable
-
     # this is the SmsGateway provided by this module
     # Note that it will be ignored if you provide your own SmsGateway in Decidim.sms_gateway_service
-    config_accessor :sms_gateway_service do
-      Decidim::Env.new("AD_SMS_GATEWAY_SERVICE", "Decidim::ActionDelegator::SmsGateway").to_s
-    end
+    mattr_accessor :sms_gateway_service,
+                   default: Decidim::Env.new("AD_SMS_GATEWAY_SERVICE", "Decidim::ActionDelegator::SmsGateway").to_s
 
     # The default expiration time for the integrated authorization
     # if zero, the authorization won't be registered
-    config_accessor :authorization_expiration_time do
-      Decidim::Env.new("AD_AUTHORIZATION_EXPIRATION_TIME").presence&.to_i || 3.months
-    end
+    mattr_accessor :authorization_expiration_time,
+                   default: (Decidim::Env.new("AD_AUTHORIZATION_EXPIRATION_TIME").presence&.to_i || 3.months)
 
     # Put this to false if you don't want to allow administrators to invite users not registered
     # in the platform when uploading a census (inviting users without permission can be a GDPR offense).
-    config_accessor :allow_to_invite_users do
-      Decidim::Env.new("AD_ALLOW_TO_INVITE_USERS", true).present?
-    end
+    mattr_accessor :allow_to_invite_users,
+                   default: Decidim::Env.new("AD_ALLOW_TO_INVITE_USERS", true).present?
 
     # If true, tries to automatically authorize users when they log in with the "Corporate Governance Verifier"
     # Note that this is only possible when the verifier is configured to use only the email (if SMS is required, the user will have to do the standard verification process)
-    config_accessor :authorize_on_login do
-      Decidim::Env.new("AD_AUTHORIZE_ON_LOGIN", true).present?
-    end
+    mattr_accessor :authorize_on_login,
+                   default: Decidim::Env.new("AD_AUTHORIZE_ON_LOGIN", true).present?
 
     # used for comparing phone numbers from a census list and the ones introduced by the user
     # the phone number will be normalized before comparing it so, for instance,
     # if you have a census list with  +34 666 666 666 and the user introduces 0034666666666 or 666666666, they will be considered the same
     # can be empty or null if yo don't want to check different combinations of prefixes
-    config_accessor :phone_prefixes do
-      Decidim::Env.new("AD_PHONE_PREFIXES", "+34,0034,34").to_array
-    end
+    mattr_accessor :phone_prefixes,
+                   default: Decidim::Env.new("AD_PHONE_PREFIXES", "+34,0034,34").to_array
 
     # The regex for validating phone numbers
-    config_accessor :phone_regex do
-      Decidim::Env.new("AD_PHONE_REGEX", '^\d{6,15}$').to_s # 6 to 15 digits
-    end
+    mattr_accessor :phone_regex,
+                   default: Decidim::Env.new("AD_PHONE_REGEX", '^\d{6,15}$').to_s # 6 to 15 digits
   end
 end
 
